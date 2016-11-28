a4ff011280480ea2413322871b6ef1c30de1e0af
Max Froumentin <max@froumentin.net>
2016-11-28 16:12:56 +0000

    Add govuk-elements and compile

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -18,6 +18,13 @@ cd assets
 GOVUK_TEMPLATE_VERSION=0.19.1
 curl -kL "https://github.com/alphagov/govuk_template/releases/download/v${GOVUK_TEMPLATE_VERSION}/django_govuk_template-${GOVUK_TEMPLATE_VERSION}.tgz" | tar xz ./govuk_template
 
+# govuk elements
+cd /vagrant
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install govuk-elements-sass gulp gulp-sass
+gulp styles
+
 cd /vagrant/src
 ./manage.py migrate
 ./manage.py runserver 0.0.0.0:8000
