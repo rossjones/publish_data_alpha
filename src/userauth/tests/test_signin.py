5d1569a94281fa289d922b71238dc47aa8c77084
Ross Jones <ross@servercode.co.uk>
2016-12-06 13:55:13 +0000

    Some signin/signout tests
    Actually makes requests to the django views so we can test that the
    flow works.

---
+++ src/userauth/tests/test_signin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from ckan_proxy.util import test_user_key
+
+class SigninTestCase(TestCase):
+
+    def setUp(self):
+        self.test_user = get_user_model().objects.create(
+            email="test-signin@localhost",
+            username="Test User Signin",
+            apikey=test_user_key()
+        )
+        self.test_user.set_password("password")
+        self.test_user.save()
+
+    def test_signin(self):
+        response = self.client.post(reverse('signin'), {
+            "email": "test-signin@localhost",
+            "password": "password"
+        })
+        assert response.status_code == 302
+        assert response.url == '/'
+
+
+    def test_signin_fail(self):
+        response = self.client.post(reverse('signin'), {
+            "email": "no",
+            "password": "nope"
+        })
+        assert response.status_code == 200
+        assert 'Login failed' in str(response.content)
+
+    def test_signin_empty(self):
+        response = self.client.post(reverse('signin'), {
+            "email": "",
+            "password": ""
+        })
+        assert response.status_code == 200
+        # Assert error message on page
+
