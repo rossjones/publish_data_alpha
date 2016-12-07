7ba56668df8ad4e0eb64ead14c7a06f3464d2b6c
Ross Jones <ross@servercode.co.uk>
2016-12-07 16:08:51 +0000

    Adds a very basic tasks app
    Contains a model for persistence of tasks and also recording which users
    have previous skipped a task. Also has tests.
    
    Requires a migration

---
+++ src/tasks/models.py
@@ -0,0 +1,45 @@
+from django.utils.translation import ugettext as _
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from django.db import models
+
+TASK_CATEGORIES = (
+    ("update",   _("Update datasets"), ),
+    ("fix",      _("Fix datasets"),    ),
+    ("improve",  _("Improve datasets"),),
+    ("accounts", _("Manage accounts"), ),
+)
+
+
+class Task(models.Model):
+    """ Represents a task that should be completed by an organisation.
+    All users who have the reuquired permission will see the task, and
+    those that choose to Skip it will not be shown it on future requests.
+    """
+    # The short name of the owning organisation.
+    owning_organisation = models.CharField(max_length=128)
+
+    # The name of the permission required to execute this task
+    # Users should not be shown the tasks where they don't have
+    # the required permission.
+    required_permission_name = models.CharField(
+        max_length=128,
+        default="",
+        blank=True
+    )
+
+    description = models.CharField(max_length=64)
+    category = models.CharField(max_length=20, choices=TASK_CATEGORIES)
+
+    def label_text(self):
+        """ The info label to display about a task """
+        if self.category == "update":
+            return _("Overdue")
+        return _("6 days ago")
+
+
+class UserHiddenTask(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    task = models.ForeignKey(Task)
+
