732adf35b55ea69f5a2d27a41afa0191755b9115
Ross Jones <ross@servercode.co.uk>
2016-12-09 15:58:00 +0000

    Implements paging in the Manage Data page.
    Because we are pulling data from two different sources, but wanting to
    sort it by a common field, this gets complicated very quickly.
    
    To fix the problem I've decided that we'll pull the maximum number
    needed, merge and sort, and then return a specific subset.  ``
    
    Requires a pip install (requirements change)

--- src/publish_data/views.py
+++ src/publish_data/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -16,7 +18,17 @@ def manage_data(request):
     # TODO: Determine default sort order, most recent first with drafts
     # and published interspersed? Paging the remote datasets will be a
     # pain
+    page = 1
+    try:
+        page = int(request.GET.get('page'))
+    except:
+        page = 1
 
-    total, datasets = dataset_list(request.user)
+    total, page_count, datasets = dataset_list(request.user, page)
 
-    return render(request, "manage.html", {"datasets": datasets, "total": total})
+    return render(request, "manage.html", {
+        "datasets": datasets,
+        "total": total,
+        "page_range": range(1, page_count),
+        "current_page": page
+    })
