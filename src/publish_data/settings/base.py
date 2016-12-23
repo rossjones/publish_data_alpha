8c95241d1c3dd98fa42d64adcee6aa34fb73a970
Ross Jones <ross@servercode.co.uk>
2016-12-23 12:15:34 +0000

    Uses env-var (DATABASE_URL) to load database config
    If DATABASE_URL does not exist, uses the current sqlite setup.
    
    See https://github.com/kennethreitz/dj-database-url for more details
    on the DATABASE_URL format

--- src/publish_data/settings/base.py
+++ src/publish_data/settings/base.py
@@ -92,12 +92,17 @@ WSGI_APPLICATION = 'publish_data.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
