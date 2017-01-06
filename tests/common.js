1ea6ec3b9fa7ff93171fbc0c19369a2f99771fb7
Max Froumentin <max@froumentin.net>
2017-01-06 16:50:20 +0000

    Add test for javascript open/close

--- tests/common.js
+++ tests/common.js
@@ -7,45 +7,44 @@ var extended = function(browser) {
       .click('//' + element + '[contains(text(), "'+text+'")]')
       .useCss();
   };
-    browser.clickOnLink = function(text) {
-      return this.clickOn('a', text);
-    };
-    browser.clickOnButton = function(text) {
-      return this.clickOn('button', text);
-    };
-    browser.selectRadioButton = function(text) {
-      return this
-        .useXpath()
-        .click('//label[contains(span/text(), "'+text+'")]')
-        .useCss();
-    };
-    browser.submitFormAndCheckNextTitle = function(title) {
-      return this
-        .submitForm('form')
-        .waitForElementVisible('h1', waitTimeout)
-        .assert.containsText('h1', title);
-    };
-    browser.clickAndCheckNextTitle = function(linkText, title) {
-      return this
-        .clickOnLink(linkText)
-        .waitForElementVisible('h1', waitTimeout)
-        .assert.containsText('h1', title);
-    };
-    browser.checkError = function(text) {
-      return this
-        .assert.containsText('ul.error-summary-list', text);
-    };
+  browser.clickOnLink = function(text) {
+    return this.clickOn('a', text);
+  };
+  browser.clickOnButton = function(text) {
+    return this.clickOn('button', text);
+  };
+  browser.selectRadioButton = function(text) {
+    return this
+      .useXpath()
+      .click('//label[contains(span/text(), "'+text+'")]')
+      .useCss();
+  };
+  browser.submitFormAndCheckNextTitle = function(title) {
+    return this
+      .submitForm('form')
+      .waitForElementVisible('h1', waitTimeout)
+      .assert.containsText('h1', title);
+  };
+  browser.clickAndCheckNextTitle = function(linkText, title) {
+    return this
+      .clickOnLink(linkText)
+      .waitForElementVisible('h1', waitTimeout)
+      .assert.containsText('h1', title);
+  };
+  browser.checkError = function(text) {
+    return this
+      .assert.containsText('ul.error-summary-list', text);
+  };
+  browser.checkFormInput = function(name) {
+    return this
+      .assert.elementPresent('input[name=' + name + ']');
+  };
+  browser.clearSetValue = function(selector, value) {
+    return this
+      .clearValue(selector).setValue(selector, value);
+  };
 
-    browser.checkFormInput = function(name) {
-      return this
-        .assert.elementPresent('input[name=' + name + ']');
-    };
-    browser.clearSetValue = function(selector, value) {
-      return this
-        .clearValue(selector).setValue(selector, value);
-    };
-
-    return browser;
+  return browser;
 }
 
 var login = function(browser, email, password) {
--- tests/tests/dashboard.js
+++ tests/tests/dashboard.js
@@ -5,15 +5,28 @@ var before = function(browser) {
   common.login(browser, process.env.USER_EMAIL, process.env.USER_PASSWORD);
 }
 
-var test_dashboard = function (browser) {
+var test_dashboard = function(browser) {
   browser
     .assert.containsText('main', 'Update datasets')
     .assert.containsText('main', 'Fix datasets')
     .end()
 };
 
+var test_dashboard_show_hide = function(browser) {
+  browser
+    .assert.visible('section:first-of-type table tr:nth-of-type(3)')
+    .assert.hidden('section:first-of-type table tr:nth-of-type(5)')
+    .click('section:first-of-type a.toggle')
+    .assert.visible('section:first-of-type table tr:nth-of-type(3)')
+    .assert.visible('section:first-of-type table tr:nth-of-type(5)')
+    .click('section:first-of-type a.toggle')
+    .assert.visible('section:first-of-type table tr:nth-of-type(3)')
+    .assert.hidden('section:first-of-type table tr:nth-of-type(5)')
+    .end();
+};
 
 module.exports = {
   before: before,
-  'Dashboard': test_dashboard
+  'Dashboard': test_dashboard,
+  'Dashboard show/hide section': test_dashboard_show_hide
 };
