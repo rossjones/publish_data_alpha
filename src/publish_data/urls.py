d6704452e3e1ee4d460994a994f143596a8b07ff
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-01-10 14:27:53 +0000

    Initial cookie bar and page

--- src/publish_data/urls.py
+++ src/publish_data/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^cookies$', views.cookies, name='cookies'),
     url(r'manage$', views.manage_data, name='manage_data'),
     url(r'^accounts/', include('userauth.urls')),
     url(r'^dataset/',  include('drafts.urls')),
