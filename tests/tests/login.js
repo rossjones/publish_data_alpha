b237b14ccb3234d96b5abd551ffec2f6349dff65
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-01-18 10:16:22 +0000

    Test for logout

--- tests/tests/login.js
+++ tests/tests/login.js
@@ -28,8 +28,25 @@ var test_failed_login = function(browser) {
     .end();
 };
 
+var test_logout = function(browser) {
+  common.extended(browser)
+    .url(process.env.APP_SERVER_URL)
+    .waitForElementVisible('body', common.waitTimeout)
+    .assert.containsText('h1', 'Publish and update data')
+    .clickOnLink('Sign in')
+    .waitForElementVisible('main', common.waitTimeout)
+    .assert.containsText('h1', 'Sign in')
+    .clearSetValue('input[name=email]', process.env.USER_EMAIL)
+    .clearSetValue('input[name=password]', process.env.USER_PASSWORD)
+    .submitFormAndCheckNextTitle('Dashboard')
+    .clickOnLink('Sign out')
+    .assert.containsText('h1', 'Publish and update data')
+    .end();
+};
+
 
 module.exports = {
   'Successful login': test_login,
+  'Successful logout': test_logout,
   'Failed login': test_failed_login
 }
