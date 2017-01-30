cea176ee235492a428c1200c33a464e4ed965e10
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-01-30 12:55:20 +0000

    Correct behaviour when loading page

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -62,14 +62,14 @@
 
     // what to do when the first 'Add another area' button is clicked
     add1: function() {
-      $(this).hide();
-      $('#location2, #add2, #del1, #del2').show();
+      $(this).addClass('js-hidden');
+      $('#location2, #add2, #del1, #del2').removeClass('js-hidden');
     },
 
     // what to do when the second 'Add another area' button is clicked
     add2: function() {
-      $(this).hide();
-      $('#location3, #add2, #del2, #del3').show();
+      $(this).addClass('js-hidden');
+      $('#location3, #add2, #del2, #del3').removeClass('js-hidden');
     },
 
     // what to do when the first 'Remove' button is clicked
@@ -80,16 +80,16 @@
 
       // second location field disappears if not third present
       if (!$('#location3').is(':visible')) {
-        $('#location2').hide();
-        $('#add1').show();
-        $('#add2').hide();
+        $('#location2').addClass('js-hidden');
+        $('#add1').removeClass('js-hidden');
+        $('#add2').addClass('js-hidden');
       } else {
-        $('#add2').show();
+        $('#add2').removeClass('js-hidden');
       }
       if (!$('#location2').is(':visible')) {
-        $('#del1').hide();
+        $('#del1').addClass('js-hidden');
       }
-      $('#location3').val('').hide();
+      $('#location3').val('').addClass('js-hidden');
     },
 
     // second Remove button is clicked
@@ -97,27 +97,37 @@
       $('#id_location2').val($('#id_location3').val());
       $('#id_location3').val('');
       if (!$('#location3').is(':visible')) {
-        $('#location2, #add2, #del1, #del2').hide();
-        $('#add1').show();
+        $('#location2, #add2, #del1, #del2').addClass('js-hidden');
+        $('#add1').removeClass('js-hidden');
       } else {
-        $('#add2').show();
+        $('#add2').removeClass('js-hidden');
       }
-      $('#location3').hide();
+      $('#location3').addClass('js-hidden');
     },
 
     del3: function() {
       $('#id_location3').val('');
-      $('#location3').hide();
-      $('#add2').show();
+      $('#location3').addClass('js-hidden');
+      $('#add2').removeClass('js-hidden');
     },
 
     init: function(params) {
-      $('#location2, #location3').hide();
-      $('#add1').on('click', this.add1).show();
+      if ($('#id_location2').val()) {
+        $('#add1').addClass('js-hidden');
+        $('#location2, #add2, #del2').removeClass('js-hidden');
+      }
+
+      if ($('#id_location3').val()) {
+        $('#add2').addClass('js-hidden');
+        $('#location3, #del3').removeClass('js-hidden');
+      }
+
+      $('#add1').on('click', this.add1);
       $('#add2').on('click', this.add2);
       $('#del1').on('click', this.del1);
       $('#del2').on('click', this.del2);
       $('#del3').on('click', this.del3);
+
       $(params.selector).typeahead(this.options, this.sourceOptions);
     },
   };
