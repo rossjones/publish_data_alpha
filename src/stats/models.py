b591c96236d8320e02acd9e1c5a858073972d486
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-02-08 11:19:08 +0000

    Internationalise back-end code Closes #54 Closes #48

--- src/stats/models.py
+++ src/stats/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 class OrganisationStatistic(models.Model):
     organisation_name = models.CharField(max_length=64)
     dataset_title = models.CharField(max_length=256)
-    subject_title = models.CharField(max_length=64, default="Downloads")
+    subject_title = models.CharField(max_length=64, default=_("Downloads"))
     value = models.IntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
     direction = models.CharField(max_length=4)
