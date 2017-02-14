006987bca4271c7d7bf86d6553874ad816784fe2
Ross Jones <ross@servercode.co.uk>
2017-02-14 12:30:19 +0000

    Fix the user admin form so we can create users

--- src/userauth/admin.py
+++ src/userauth/admin.py
@@ -1,7 +1,9 @@
 from django.utils.translation import ugettext as _
 
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from userauth.models import PublishingUser
 
@@ -11,12 +13,26 @@ class OrganisationsInline(admin.TabularInline):
     extra = 1
 
 
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label= ("Password"),
+        help_text= ("Raw passwords are not stored, so there is no way to see "
+                    "this user's password, but you can change the password "
+                    "using <a href=\"../password/\">this form</a>."))
+
+    class Meta:
+        model = PublishingUser
+        exclude = ()
+
+    def clean_password(self):
+        return self.initial["password"]
+
 
 class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
     model = PublishingUser
     inlines = [OrganisationsInline]
     fieldsets = (
-        (None, {'fields': ('email', 'apikey')}),
+        (None, {'fields': ('email', 'apikey', 'username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
@@ -25,10 +41,11 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'password1',
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1',
                        'password2')}
          ),
     )
+
 
 
 admin.site.register(PublishingUser, UserAdmin)
