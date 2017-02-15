178279746939d4d5930fc81eb5baf96b962b52d9
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-02-15 17:09:28 +0000

    Production dir for heroku

--- src/publish_data/settings/prod.py
+++ src/publish_data/settings/prod.py
@@ -1,3 +1,12 @@
 from .base import *
 
 DEBUG = False
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "assets/govuk_template/static"),
+    os.path.join(PROJECT_DIR, "assets"),
+]
