e8191af325b8fa729114aea7a06703ed3354abc5
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-03-24 12:13:21 +0000

    Simple robots.txt

--- src/publish_data/urls.py
+++ src/publish_data/urls.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 from django.conf.urls import url, include
 from . import views
 
@@ -10,6 +11,7 @@ urlpatterns = [
 
     url(r'^$', views.home, name='home'),
     url(r'^cookies$', TemplateView.as_view(template_name='cookies.html'), name='cookies'),
+    url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow: /\n", content_type="text/plain"), name="robots_file"),
     url(r'^manage/$', views.manage_my_data, name='manage_my_data'),
     url(r'^manage/organisation/$', views.manage_org_data, name='manage_org_data'),
     url(r'^accounts/', include('userauth.urls')),
