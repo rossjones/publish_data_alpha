461370dd6cc21cd53554d46a7fd505e571bed617
Ross Jones <ross@servercode.co.uk>
2017-03-24 11:04:17 +0000

    An API.
    Provides a REST API for working with datasets and organisations. This is
    meant to be used for publishing and updating datasets. It is currently
    not-authenticated (but will use the oauth provider) and doesn't
    implement search.

---
+++ src/api/api_def.py
@@ -0,0 +1,46 @@
+from django.conf.urls import url, include
+from datasets.models import Dataset, Organisation, Datafile
+from rest_framework import serializers, viewsets
+
+class DatafileSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Datafile
+        exclude = ('month', 'year', 'quarter', 'dataset')
+
+class OrganisationSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Organisation
+        exclude = ('users',)
+        extra_kwargs = {
+            'url': {'lookup_field': 'name'}
+        }
+
+
+class DatasetSerializer(serializers.HyperlinkedModelSerializer):
+    #organisation = serializers.SlugRelatedField(
+    #    many=False,
+    #    read_only=True,
+    #    slug_field='name'
+    # )
+    organisation = OrganisationSerializer(many=False, read_only=True)
+    files = DatafileSerializer(many=True, read_only=False)
+
+    class Meta:
+        model = Dataset
+        exclude = ('owner', 'creator', 'legacy_metadata')
+        extra_kwargs = {
+            'url': {'lookup_field': 'name'}
+        }
+
+
+class DatasetViewSet(viewsets.ModelViewSet):
+    queryset = Dataset.objects.all()
+    serializer_class = DatasetSerializer
+    lookup_field = 'name'
+
+class OrganisationViewSet(viewsets.ModelViewSet):
+    queryset = Organisation.objects.all()
+    serializer_class = OrganisationSerializer
+    lookup_field = 'name'
