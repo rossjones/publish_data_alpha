45e0d60bb3f0373ca00d17c62378eebff4b2ce70
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-04-11 18:21:13 +0100

    Use sqlite for running tests

--- src/publish_data/settings/test.py
+++ src/publish_data/settings/test.py
@@ -1,5 +1,13 @@
 from .base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    }
+}
+
+USE_TZ = True
 
 ES_INDEX = 'data_discovery_test'
 
